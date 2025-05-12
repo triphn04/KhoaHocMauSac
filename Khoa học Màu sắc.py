0c63fd90cefaebfbfeb5f6c3fb47b5b84cbb4981
triphn04 <triphn04@gmail.com>
2025-05-12 16:27:33 +0700

    Add files via upload

---
+++ Khoa học Màu sắc.py
@@ -0,0 +1,9 @@
+﻿import streamlit as st
+st.set_page_config(
+    page_title="Khoa học Màu sắc", layout="wide"
+)
+
+st.write("### Chào mừng bạn đến với project Khoa học Màu sắc của tụi mình!")
+st.write("# Mình tên là Phạm Nguyễn Hữu Trí, MaSV 22158100")
+st.write("# Mình tên là Đào Nguyễn Ngọc Linh,MaSV 22158064")
+
