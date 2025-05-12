125ff5d342c32654055ef340928408e92a476367
triphn04 <triphn04@gmail.com>
2025-05-12 16:18:09 +0700

    Update Khoa học Màu sắc.py

--- Khoa học Màu sắc.py
+++ Khoa học Màu sắc.py
@@ -3,5 +3,7 @@ st.set_page_config(
     page_title="Khoa học Màu sắc", layout="wide"
 )
 
-st.write("### Chào mừng bạn đến với project Khoa học Màu sắc của mình!")
-st.write("# Mình tên là Võ Tự Thanh, MaSV 22158139")
+st.write("### Chào mừng bạn đến với project Khoa học Màu sắc của tụi mình!")
+st.write("# Thành viên nhóm mình gồm:"
+st.write("# Phạm Nguyễn Hữu Trí, MaSV 22158100")
+st.write("# Đào Nguyễn Ngọc Linh,MaSV 22158064")
