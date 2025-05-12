027fb665dc9010ab8b85151d76f07bfc749a379e
triphn04 <triphn04@gmail.com>
2025-05-12 16:27:14 +0700

    Delete Khoa học Màu sắc.py

--- Khoa học Màu sắc.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-st.set_page_config(
-    page_title="Khoa học Màu sắc", layout="wide"
-)
-
-st.write("### Chào mừng bạn đến với project Khoa học Màu sắc của tụi mình!")
-st.write("# Thành viên nhóm mình gồm:"
-st.write("# Phạm Nguyễn Hữu Trí, MaSV 22158100")
-st.write("# Đào Nguyễn Ngọc Linh,MaSV 22158064")
